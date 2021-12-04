da3706b3ae123cfd22554a82c8e8cf19ec214a53
DarkLordOfDeadstiny <patr7500@gmail.com>
2021-12-04 14:23:13 +0100

    started on bl parsing

--- commands/addToList.js
+++ commands/addToList.js
@@ -8,39 +8,98 @@ const { Util } = require('discord.js');
 // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et sollicitudin felis. Vestibulum pretium metus at pellentesque iaculis. Fusce ornare purus id efficitur eleifend. Mauris volutpat ligula tellus, ac maximus enim tempor vel. Morbi ut porttitor augue, eget congue urna. Curabitur venenatis gravida mauris vel hendrerit. Vivamus a dolor ex. Ut mollis, ipsum semper sollicitudin ultrices, dolor orci efficitur neque, in placerat risus velit et diam. Etiam ornare nunc ut sapien imperdiet, eu venenatis massa laoreet. Duis fermentum turpis ac lectus sagittis aliquam. Quisque ut magna dolor. Donec tristique mi semper dui luctus cursus. Ut quis sollicitudin lacus, scelerisque vulputate eros. Aenean ac nibh volutpat, ultrices tortor vel, fringilla diam. Mauris pharetra, justo id dapibus porttitor, velit arcu consequat nisi, quis sagittis lacus mauris nec eros.\n'+
 // 'Curabitur sed diam elementum, egestas est eget, finibus risus. Curabitur eleifend ligula velit, eget fringilla nulla fringilla eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent mollis tortor et felis placerat accumsan. Fusce efficitur malesuada sapien, quis vulputate dolor commodo consectetur. Proin ullamcorper semper ipsum, ac tincidunt tortor porttitor quis. Nullam eros ipsum, pellentesque sed quam fringilla, pretium pharetra elit.';
 
+const chatListIds = [];
+const blacklist = new Map([
+    ['A', []],
+    ['B', []],
+    ['C', []],
+    ['D', []],
+    ['E', []],
+    ['F', []],
+    ['G', []],
+    ['H', []],
+    ['I', []],
+    ['J', []],
+    ['K', []],
+    ['L', []],
+    ['M', []],
+    ['N', []],
+    ['O', []],
+    ['P', []],
+    ['Q', []],
+    ['R', []],
+    ['S', []],
+    ['T', []],
+    ['U', []],
+    ['V', []],
+    ['W', []],
+    ['X', []],
+    ['Y', []],
+    ['Z', []]
+]);
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('add')
-		.setDescription('Adds the user to the blacklist'),
-	async execute(interaction, channel) {
+    data: new SlashCommandBuilder()
+        .setName('addAll')
+        .setDescription('Adds the user to the blacklist'),
+    async execute(interaction, channel) {
         if (interaction.channel.name !== 'blacklist') {
             console.log('channel was not a blacklist');
             await interaction.reply('channel is not a blacklist');
             return;
         }
         const names = await getAllMessages(channel);
-        
+
+        //-------- save blacklist to file here -------
+
+        //delete old messages
+        deleteOldMessages(channel);
+
+
         // Util.splitMessage(message).forEach(async e => {
         //     await channel.send(e);
         // });
-        
+
         //FIXME
         //might take more than 3 seconds so if it starts failing that might be why
         //a fix does exist with an other type of reply
-        await interaction.reply('aded to list ' + names[0]); 
-	},
+        await interaction.reply('aded to list ' + names[0]);
+    },
 };
 
 
 async function getAllMessages(channel) {
     let names = [];
-    await channel.messages.fetch({ limit: 10 }).then(messages => {
+    await channel.messages.fetch().then(messages => {
         console.log(`Received ${messages.size} messages`);
         //Iterate through the messages here with the variable "messages".
         messages.forEach(message => {
+            if (message.content.startsWith('--')) {
+                addOldToBl(message.content);
+                chatListIds.push(message.id);
+            } else if (message.content.toLowerCase().startsWith('add ')) {
+                addToBl(message.content.slice(4));
+                chatListIds.push(message.id);
+            }
             console.log(message.content);
-            names.push(message.content);
         });
-      });
-    return names;
+    });
+    return names.reverse();
+}
+
+async function deleteOldMessages(channel) {
+    chatListIds.forEach(id => cha)
+}
+
+function addOldToBl(list) {
+    list.split('\n').forEach(name => addToBl(name));
+}
+
+function addToBl(name) {
+    const first = name.charAt(0).toUpperCase();
+    blacklist.get(first).push(name.trim());
+}
+
+async function postBl(channel) {
+    blacklist.forEach()
 }
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('interactionCreate', async interaction => {
     const command = client.commands.get(interaction.commandName);
 
 	if (!command) return;
-
+	
 	try {
 		await command.execute(interaction, interaction.channel);
 	} catch (error) {
