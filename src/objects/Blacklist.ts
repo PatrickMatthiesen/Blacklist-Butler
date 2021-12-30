bd117d2ee5311275673a8b286ea03f6d564e4ac5
DarkLordOfDeadstiny <patr7500@gmail.com>
2021-12-30 22:13:04 +0100

    works

--- src/objects/Blacklist.ts
+++ src/objects/Blacklist.ts
@@ -1,17 +1,18 @@
-import { Message, TextBasedChannels } from "discord.js";
-import * as fs from "fs"
+import { channel } from 'diagnostics_channel';
+import { Channel, Message, TextBasedChannels, TextChannel } from "discord.js";
+import * as fs from "fs";
 
 export class Blacklist {
 
     private blacklist: Map<string, string[]>; // the content of the old messages
     private oldMessages: Map<string, Message>; //the old messages for editing the messages
-    private toDelete: string[]; //list of ids to delete
+    private toDelete: Message[]; //list of ids to delete
     private refresh: boolean;
     private channel: TextBasedChannels;
 
     constructor(toRefresh: boolean, channel: TextBasedChannels) {
-        this.blacklist = new Map<string, string[]>()
-        this.oldMessages = new Map<string, Message>()
+        this.blacklist = new Map<string, string[]>();
+        this.oldMessages = new Map<string, Message>();
         this.toDelete = [];
         this.refresh = toRefresh;
         this.channel = channel;
@@ -19,49 +20,18 @@ export class Blacklist {
 
     addOld(message: Message) {
         message.content.split('\n').forEach(name => this.add(name));
-        this.oldMessages.set(message.content.charAt(2), message)
+        this.oldMessages.set(message.content.charAt(4), message);
         if (message.author.id != '915952587273023508') { // if it isn't this bot that send the message then delete the message
-            this.toDelete.push(message.id)
-            this.refresh = true
+            this.toDelete.push(message);
+            this.refresh = true;
         }
     }
-
-    // async addNew(name: string): Promise<boolean> { //implement refresh --------------------------------        
-    //     const char = name.charAt(0);
-
-    //     var mess: string[] = [];
-
-
-    //     //add something that finds the old message id from a json file
-    //     await this.channel.messages.fetch().then(messages => {
-    //         messages.forEach(msg => {
-    //             if (!this.refresh && msg.author.id != '915952587273023508') {
-    //                 this.refresh = true;
-    //             }
-    //             if (msg.content.startsWith('--' + char)) {
-    //                 msg.content.split('\n').forEach(name => mess.push(name));
-    //             }
-    //             mess.push(name)
-    //             mess.sort()
-    //             if (this.refresh) {
-    //                 this.add(name)
-    //                 return;
-    //             } else {
-    //                 msg.edit(mess.join('\n'))
-    //             }
-    //             console.log(mess);
-    //             return;
-    //         });
-    //     });
-
-    //     return this.refresh;
-    // }
 
     add(message: string) {
         let char: string;
 
         if (new RegExp(`^--[A-Z]--`).test(message)) {
-            char = message.charAt(2).toLocaleUpperCase()
+            char = message.charAt(4).toLocaleUpperCase();
         } else char = message.charAt(0).toLocaleUpperCase();
 
         if (this.blacklist.has(char)) {
@@ -69,62 +39,116 @@ export class Blacklist {
             return;
         }
 
-        this.blacklist.set(char, new Array(message))
+        this.blacklist.set(char, new Array(message));
     }
 
-    updateOldMessages() {
-        this.oldMessages.forEach(async (message, key) =>
-            await this.updateMessage(
-                message,
-                this.blacklist.get(key)
-            )
-        )
+    async addOne(name: string) {
+        console.log('adds ' + name);
+
+        this.add(name);
+        this.sort();
+
+        const char = name.charAt(0).toUpperCase();
+
+        await this.updateMessage(char);
+
+
+        this.saveBlacklistToFile();
+        return true;
     }
 
-    async updateMessage(message: Message | undefined, list: string[] | undefined) {
-        if (!list) return; //if undifined, nan, null, '', 0 or false
-        console.log("list is ok");
-        
-        list.sort();
+    async removeOne(name: string) {
+        console.log('removes ' + name);
 
-        try {
-            message?.edit(list.join('\n'))
-        } catch (err) {
-            console.log(err);
-            
+        this.remove(name);
+        this.sort();
+
+        const char = name.charAt(0).toUpperCase();
+
+        await this.updateMessage(char);
+
+
+        this.saveBlacklistToFile();
+        return true;
+    }
+
+    remove(name: string) {
+        const char = name.charAt(0).toUpperCase();
+
+        const list = this.blacklist.get(char) ?? [];
+        if (list.length === 0) return;
+
+        const index = list.indexOf(name);
+        console.log('removed ' + list.splice(index, 1));
+    }
+
+
+    async updateAllMessages() {
+        this.sort();
+        for (const key of this.oldMessages.keys()) {
+            await this.updateMessage(key);
         }
+    }
 
-        console.log(`updated message ${message?.id} with new content`);
+    async updateMessage(char: string) {
+        const msgid = this.oldMessages.get(char)?.id;
+        const names = this.blacklist.get(char);
+        if (!msgid || !names) return false;
+
+        const msg = await this.channel.messages.fetch(msgid);
+
+        msg.edit(names.join('\n'));
+        console.log('eddited message ' + msgid);
     }
 
     async writeToChat() {
-        this.blacklist = new Map([...this.blacklist].sort())
-        this.blacklist.forEach(item => {
-            console.log(item);
-            item.sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()))
-            const joined = item.join('\n')
-            this.channel.send(joined)
-        })
+        this.sort();
+        for (const item of this.blacklist.values()) {
+            const joined = item.join('\n');
+            await this.sendAndSave(joined);
+        }
+        console.log('messages sent');
     }
 
-    async cleanChat(includeOld: boolean = false) {
+    async sendAndSave(text: string) {
+        const msg = await this.channel.send(text);
+        this.oldMessages.set(text.charAt(4), msg);
+    }
+
+    async cleanChat(includeOld = false) {
         if (includeOld) {
-            this.oldMessages.forEach(async msg => await msg.delete())
+            this.oldMessages.forEach(async msg => await msg.delete());
+            console.log('deleted old messages');
         }
 
-        this.toDelete.forEach(async id => await (await this.channel.messages.fetch(id)).delete())
+        this.toDelete.forEach(async msg => await msg.delete());
+        console.log('deleted messages');
+
     }
 
-    // getToDelete(): string {
-    //     return this.oldMessages.toString().replaceAll(',', '\n'); //yeah no that wont work my friend
-    // }
+    addToDelete(msg: Message) {
+        this.toDelete.push(msg);
+    }
 
     getRefresh(): boolean {
         return this.refresh;
     }
 
-    saveToFile() {
-        fs.writeFile('Blacklist.json', JSON.stringify([...this.blacklist], null, 4), (err: any) => {
+    sort() {
+        this.blacklist = new Map([...this.blacklist].sort());
+        this.blacklist.forEach(list => {
+            list.sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()));
+        });
+        console.log('sorted the list');
+
+        // for (const item of this.blacklist.values()) {
+        //     item.sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()));
+        // }
+    }
+
+    saveBlacklistToFile() {
+        this.sort();
+        fs.writeFile('Blacklist.json', JSON.stringify([...this.blacklist], null, 4), err => {
             if (err) {
                 throw err;
             }
@@ -132,25 +156,52 @@ export class Blacklist {
         });
     }
 
-    loadFromFile() {
-        const data = fs.readFileSync('Blacklist.json', 'utf-8');
-
-        // parse JSON object
-        this.blacklist = new Map(JSON.parse(data)); // not posible if the file is empty
-        
-        // print JSON object
-        console.log('the parsed map object:\n' + this.blacklist);
-
-        //  (err: NodeJS.ErrnoException | null, data: string) => {
+    saveMessageIdsToFile() {
+        fs.writeFileSync('messageIds.json', JSON.stringify([...this.oldMessages], null, 4));
+        // , (err: any) => {
         //     if (err) {
         //         throw err;
         //     }
-
         // });
+        console.log("message ids is saved as JSON data");
+    }
+
+    loadFromFile() {
+        this.loadBlacklist();
+        this.loadMessageIds();
+    }
+
+    private loadBlacklist() {
+        try {
+            const data = fs.readFileSync('Blacklist.json', 'utf-8');
+
+            // parse JSON object
+            this.blacklist = new Map(JSON.parse(data)); // not posible if the file is empty
+
+            // print JSON object
+            console.log('the parsed messages map object:\n' + this.blacklist);
+            console.log(this.blacklist.get('A'));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    private loadMessageIds() {
+        try {
+            const idMap = fs.readFileSync('messageIds.json', 'utf-8');
+
+            // parse JSON object
+            this.oldMessages = new Map(JSON.parse(idMap)); // not posible if the file is empty
+
+            // print JSON object
+            console.log('the parsed messages map object:\n' + this.blacklist);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     isEmpty(): boolean {
-        return this.blacklist.size == 0
+        return this.blacklist.size == 0;
     }
 
 }
